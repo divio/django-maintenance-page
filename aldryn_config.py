d1b1be03be4064ac2f136b7298353392a86e34a8
Filipe Waitman <filwaitman@gmail.com>
2018-02-22 11:53:38 -0300

    Add aldryn_config.py (#4)

---
+++ aldryn_config.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+from functools import partial
+
+from aldryn_client import forms
+
+from django_maintenance_page import conf
+
+
+class Form(forms.BaseForm):
+    def to_settings(self, data, settings):
+        from aldryn_addons.utils import djsenv
+        env = partial(djsenv, settings=settings)
+
+        release_identifier = env(conf.MAINTENANCE_ENV_VAR, required=True)
+        migration_commands = settings.setdefault('MIGRATION_COMMANDS', [])
+        migration_commands.insert(0, 'python manage.py create_release {}'.format(release_identifier))
+        return settings
--- django_maintenance_page/conf.py
+++ django_maintenance_page/conf.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
-import distutils
+from distutils.util import strtobool
 import os
 
-MAINTENANCE_MODE = bool(distutils.util.strtobool(os.environ.get('DJANGO_MAINTENANCE_MODE', '0')))
+MAINTENANCE_MODE = bool(strtobool(os.environ.get('DJANGO_MAINTENANCE_MODE', '0')))
 MAINTENANCE_ENV_VAR = os.environ.get('DJANGO_MAINTENANCE_ENV_VAR', 'DJANGO_MAINTENANCE_PAGE_RELEASE')
 MAINTENANCE_STATUS_CODE = int(os.environ.get('DJANGO_MAINTENANCE_STATUS_CODE', '503'))
