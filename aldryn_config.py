248884d102ec113d1a7cf4ca49f0d9a5f43ffab3
Paulo Alvarado <commonzenpython@gmail.com>
2018-02-28 11:54:12 -0500

    Don't add release command when building docker image

--- aldryn_config.py
+++ aldryn_config.py
@@ -9,9 +9,16 @@ class Form(forms.BaseForm):
     def to_settings(self, data, settings):
         from aldryn_addons.utils import djsenv
         from django_maintenance_page import conf
+
         env = partial(djsenv, settings=settings)
+
+        if env('DJANGO_MODE') == 'build':
+            return settings
 
         release_identifier = env(conf.MAINTENANCE_ENV_VAR, required=True)
         migration_commands = settings.setdefault('MIGRATION_COMMANDS', [])
-        migration_commands.insert(0, 'python manage.py create_release {}'.format(release_identifier))
+        migration_commands.insert(
+            0,
+            'python manage.py create_release {}'.format(release_identifier),
+        )
         return settings
